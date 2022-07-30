d92c5fd4696f8e73e1f9caee083c22ec34ce9fb7
zhangwuji28 <zhangwuji28@163.com>
2022-07-30 18:45:42 +0800

    提交测试3

--- src/main/java/com/guo/git/Test.java
+++ src/main/java/com/guo/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("测试git1");
         System.out.println("测试git2");
+        System.out.println("测试git3");
     }
 }
